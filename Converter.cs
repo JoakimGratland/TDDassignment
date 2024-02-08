cfc64194ef8b46d32efbb072d50070e18dbbfd2d
unknown <joakimsg@uia.no>
2024-02-09 00:38:40 +0100

    unsure if i did the program right but i added it, will figure out while i add a way to test it.

--- Converter.cs
+++ Converter.cs
@@ -1,5 +1,7 @@
     using System;
 
+   namespace converter
+   {
     public class converter
     {
         public static double ConvertToMilimeters(double inches)
@@ -16,5 +18,51 @@
         {
             return inches * 0.0254;
         }
-        
     }
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                Console.WriteLine("write: dotnet run <number> <-unit>");
+                return;
+            }
+
+            if (args[1] != "-cm" && args[1] != "-m" && args[1] != "-mm")
+            {
+                Console.WriteLine("please write a valid unit: -cm, -m, -mm");
+                Console.WriteLine("example: dotnet run 5 -cm");
+                return;
+            }
+
+            
+            if (!double.TryParse(args[0], out inches))
+            {
+                Console.WriteLine("please write a number.");
+                return;
+            }
+    
+            switch (args[1])
+            {
+                case "-mm":
+                    result = Converter.ConvertToMillimeters(inches);
+                    Console.WriteLine($"{result} mm");
+                    break;
+                case "-cm":
+                    result = Converter.ConvertToCentimeters(inches);
+                    Console.WriteLine($"{result} cm");
+                    break;
+                case "-m":
+                    result = Converter.ConvertToMeters(inches);
+                    Console.WriteLine($"{result} m");
+                    break;
+                default:
+                    Console.WriteLine("Invalid unit. Please use -mm, -cm, or -m.");
+                    break;
+            }
+        }
+    }
+
+   }
